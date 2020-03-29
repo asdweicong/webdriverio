3e8fefb521b8dea3a73922bdd29470e8f8e2f785
weicon <1418066959@qq.com>
2020-03-29 21:48:45 +0800

    'add'

--- test_.js
+++ test_.js
@@ -19,12 +19,32 @@ function simulateClick(btn) { // 模拟 浏览器的鼠标点击事件
 }
 var money = document.getElementsByClassName("money-box");
 money[0].setAttribute('id', 'money');
+
+
 var allNum = document.getElementsByClassName('color-ball')//所有数据
 var allContent = document.getElementsByClassName('ball-content');//所有可操作数据
 simulateClick(allContent[0]);
+
 setTimeout(function () {
-    document.getElementById('money').focus();
     document.getElementById('money').value = 100;
+
+    var theOneNum = allNum[0].innerText;
+    console.log('theOneNum-第一个数', theOneNum);
+    console.log('theOneNum-第一个操作数', allContent[0]);
+    // for (let index = 0; index < allContent.length; index++) {
+    //     allContent[index].classList.remove("active-color");
+    // }
+    // allContent[2].classList.add("active-color");
+    // document.getElementById('onebox');
+    // var inner_tnmgo = document.getElementById('inner-dneri');//注意会动态改变
+    // var lis= inner_tnmgo.childNodes.item(2);
+    // simulateClick(lis)
+    // lis.classList.add('active-color');
+    // var lis_active = lis.childNodes.item(0);
+    // lis_active.classList.add('active-hover');
+    // simulateClick(lis_active)
+    // document.getElementsByClassName("money-box")[0].focus();//注意需要手动添加
+    // document.getElementsByClassName("money-box")[0].value = 100;
     setTimeout(function () {
         var bet = document.getElementsByClassName('bet');
         simulateClick(bet[0])
